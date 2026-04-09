3b184f4317d4983ccdc45c9529b40bc7a474c2fe
Arpit-dhameliya <110677638+Arpit-dhameliya@users.noreply.github.com>
2026-04-09 23:27:09 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,22 +11,27 @@ const Index = () => {
       {/* Content layer */}
       <div className="relative z-10 flex flex-col min-h-screen">
         {/* Center content */}
-        <div className="flex-1 flex flex-col items-center justify-center px-6 text-center">
+        <div className="flex-1 flex flex-col px-6 mt-4">
           {/* Logo monogram */}
           <div className="animate-fade-up mb-4">
             <img src={adLogo} alt="AD Logo" className="w-20 h-20 object-contain" />
           </div>
 
           {/* Brand name */}
-          <h1
+          { /* <h1
             className="text-4xl sm:text-5xl md:text-7xl font-bold tracking-tight mb-8 animate-fade-up-delay-1"
             style={{ fontFamily: "var(--font-display)", color: "#EBE2D5" }}
           >
             Anand Dessert
-          </h1>
+          </h1> */ }
 
+          
+        </div>
+
+        {/* Bottom bar */}
+        <div className="relative px-6 pb-6 flex flex-col sm:flex-row items-center justify-between gap-4 animate-fade-up-delay-3">
           {/* CTA Buttons */}
-          <div className="flex flex-wrap gap-4 justify-center animate-fade-up-delay-2">
+          <div className="flex flex-wrap gap-4 justify-center animate-fade-up-delay-2 mb-4">
             <a href="https://g.co/kgs/bkULVz3" className="btn-primary-glow">
               Rate Us
             </a>
@@ -34,10 +39,7 @@ const Index = () => {
               Contact Us
             </a>
           </div>
-        </div>
-
-        {/* Bottom bar */}
-        <div className="relative px-6 pb-6 flex flex-col sm:flex-row items-center justify-between gap-4 animate-fade-up-delay-3">
+          
           {/* Did you know */}
           { /* <div className="text-left">
             <p className="text-sm font-medium opacity-80 mb-1">Did you know?</p>
